1ad866408c78214fa6f8ec482801d12b58f0facb
Alexandrio Vega Bonito <alexandriovegabonito@gmail.com>
2025-11-14 13:18:05 +0700

    Merubah file pada folder migrations by Alexandrio Vega Bonito

---
+++ datahub-backend/database/migrations/2025_11_11_000003_create_slta_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('slta', function (Blueprint $table) {
+            $table->id('slta_id');
+            $table->string('nama_slta_resmi', 100)->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('slta');
+    }
+};
