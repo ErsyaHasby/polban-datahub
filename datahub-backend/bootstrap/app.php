33efbe2a944db355a0a4466216a659476ddd7f8a
Gema <gemaadzanf@gmail.com>
2025-12-11 12:35:45 +0700

    tambahin sedikit di middleware

--- datahub-backend/bootstrap/app.php
+++ datahub-backend/bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
-        ]);
+        ])->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
